8386cafeae29f9182bee62c1f5b42d330bd3cd68
Jason <insu0929@naver.com>
2022-03-14 23:58:56 +0900

    Stream(2)

--- 03 stream/Main.java
+++ 03 stream/Main.java
@@ -1,6 +1,7 @@
 package com.jason.argos;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -17,7 +18,36 @@ public class Main {
                 new Dish("pizza", true, 550, Dish.Type.Other ),
                 new Dish("prawns", false, 300, Dish.Type.FISH),
                 new Dish("salmon", false, 450, Dish.Type.FISH ));
-        
+
+        //java8 이전
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for(Dish dish : menu){
+            if(dish.getCalories() < 400){
+                lowCaloricDishes.add(dish);
+            }
+        }
+
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            @Override
+            public int compare(Dish o1, Dish o2) {
+                return Integer.compare(o1.getCalories(), o2.getCalories());
+            }
+        });
+
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for(Dish dish : lowCaloricDishes){
+            lowCaloricDishesName.add(dish.getName());
+        }
+        // 실제로 구하려고 하는 lowCalricDishNames를 얻기 위해 중간에 버리게 되는 lowCaloricDishes 라는 가비지 변수가 생긴다.
+
+
+        //java8
+        lowCaloricDishesName = menu.stream()
+                .filter(d -> d.getCalories() < 400)
+                .sorted(Comparator.comparing(Dish::getCalories))
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+
 
 
     }
